46c8e2d005065f8e2552e74ecf739e4d4a947f6e
mani <mani@belong.co>
2016-09-01 22:28:53 +0530

    Added jquery ui for date and symbol autocomplete

--- stocks/views.py
+++ stocks/views.py
@@ -1,4 +1,4 @@
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 from django.db.models import F
 from django.shortcuts import render
 
@@ -18,36 +18,25 @@ def stock_history_view(request):
     stocks_qs = StockHistory.objects.filter(is_filtered=True)
     if request.method == 'POST':
         if request.POST.get('search-btn'):
-            date_field = request.POST.get('trade-date')
+            trade_date = request.POST.get('trade-date')
+            date_field = datetime.strptime(trade_date, '%d/%m/%Y') if trade_date else None
             symbol = request.POST.get('stock_symbol')
             if symbol:
-                if not date_field:
+                if not trade_date:
                     today_date = date.today()
                     stocks = stocks_qs.filter(trade_date__range=[today_date - timedelta(25), today_date],
                                               stock__symbol=symbol.strip()) \
                         .order_by('-trade_date')
                 else:
                     stocks = stocks_qs.filter(trade_date=date_field, stock__symbol=symbol.strip())
-            else:
+            elif trade_date:
                 stocks = stocks_qs.filter(trade_date=date_field).annotate(
                     change=F('close') - F('open')).select_related('stock')
             ctx['stocks'] = stocks
-
-        # elif request.POST.get('to_watchlist'):
-        #     watchlist_stocks = request.POST.getlist('watch_list')
-        #     stocks = StockHistory.objects.filter(id__in=watchlist_stocks)
-        #     stocks.update(watch_list=True)
-        #
-        #     stocks_bydate = stocks_qs.filter(trade_date=stocks.first().trade_date) \
-        #         .annotate(change=F('close') - F('open'))
-        #
-        #     ctx['stocks'] = stocks_bydate
 
     elif request.method == 'GET':
         trade_date = stocks_qs.order_by('-trade_date').first().trade_date
         stocks = stocks_qs.filter(trade_date=trade_date)
         ctx['stocks'] = stocks
-
-        return render(request, watch_list_template, ctx)
 
     return render(request, watch_list_template, ctx)
