35b43f6737f1573a50589f00feab67b8660de2dc
mani <mani@belong.co>
2017-02-28 11:07:30 +0530

    Added custom macd

--- strategies/macd.py
+++ strategies/macd.py
@@ -1,8 +1,9 @@
 import collections
 
 import pandas as pd
-from stockstats import StockDataFrame
+from utils.stockstats import StockDataFrame
 from datetime import date, timedelta
+import datetime
 
 from stocks.models import StockHistory, Stock
 
@@ -14,21 +15,28 @@ class ProcessStragey1(object):
         self.stock = Stock.objects.get(symbol=symbol)
 
     def get_macd_data(self):
-        queryset = StockHistory.objects.filter(stock_id=self.stock.id, ).extra(
+        # trade_date__range=['2016-01-01', datetime.date.today()]
+        queryset = StockHistory.objects.filter(stock_id=self.stock.id,
+                                               ).extra(
             select={'date': 'trade_date'}).values(
             'date', 'open', 'high', 'low', 'close')
         stockdataframe = StockDataFrame.retype(pd.DataFrame.from_records(queryset))
 
         histogram = stockdataframe['macdh'].to_dict()
-        # signal_line = stockdataframe['macds'].to_dict()
-        # macd_line = stockdataframe['macd'].to_dict()
+        signal_line = stockdataframe['macds'].to_dict()
+        macd_line = stockdataframe['macd'].to_dict()
 
         macd_data = {}
         for date in histogram.keys():
-            macd_data[date] = {'histogram': histogram[date] / 2,  # 'macd_line': macd_line[date],
-                               # 'signal_line': signal_line[date]
+            macd_data[date] = {'histogram': histogram[date], 'macd_line': macd_line[date],
+                               'signal_line': signal_line[date]
                                }
+            # macd_data[date] = {'histogram': histogram[date]}
         macd_data = collections.OrderedDict(sorted(macd_data.items()))
+
+        # for date, macd_result in macd_data.iteritems():
+        #     print date, StockHistory.objects.get(trade_date=date, stock_id=self.stock.id).close, macd_result[
+        #         'macd_line'], macd_result['signal_line'], macd_result['histogram']
         return macd_data
 
     def backtest(self):
@@ -41,7 +49,8 @@ class ProcessStragey1(object):
             if index == 26:
                 prev_point = histogram
             elif index > 26:
-                if histogram > 0 and prev_point < 0:
+                # if histogram > 0 and prev_point < 0:
+                if histogram > prev_point and histogram < 0:
                     buy_signals[signal] = data[signal]
                 prev_point = histogram
 
@@ -54,40 +63,43 @@ class ProcessStragey1(object):
         for buy_date, macd in buy_signals.iteritems():
             trade_dates = filter(lambda x: x > buy_date, data)
             profit = 0
-
             try:
                 buy_price = StockHistory.objects.filter(stock_id=self.stock.id,
                                                         trade_date__gt=buy_date).first().open
-            except StockHistory.MultipleObjectsReturned:
-                print "Multiple records found", self.stock.id, buy_date
-                return
+            except AttributeError as err:
+                print err, 73
+                import ipdb;
+                ipdb.set_trace()
+                continue
 
             bought_at = buy_price
             previous_histogram = macd['histogram']
             for trade_date in trade_dates:
-                if data[trade_date]['histogram'] > previous_histogram:
-                    try:
-                        current_close = StockHistory.objects.filter(stock_id=self.stock.id,
-                                                                    trade_date__gt=trade_date).first().open
-                    except StockHistory.MultipleObjectsReturned:
-                        print "Multiple records found", self.stock.id, trade_date
-                        return
+                try:
 
-                    profit += current_close - buy_price
-                    buy_price = current_close
+                    current_open = StockHistory.objects.filter(stock_id=self.stock.id,
+                                                               trade_date__gt=trade_date).first().open
+                except AttributeError as err:
+                    print err, 86
+                    import ipdb; ipdb.set_trace()
+                    continue
+                profit += current_open - buy_price
+                buy_price = current_open
+
+                if data[trade_date]['histogram'] > previous_histogram:
                     previous_histogram = data[trade_date]['histogram']
                 else:
                     if profit > 0:
-                        n_profit += 1
+                            n_profit += 1
                     elif profit < 0:
                         n_negative += 1
                     elif profit == 0:
                         n_neutral += 1
-                    print '%s\t%s\t%.1f\t%s\t%.1f\t%.2f%%\t%.2f\t%.3f' % (
+                    print '%s\t%s\t%.1f\t%s\t%.1f\t%.2f%%\t%.2f' % (
                         (trade_date - buy_date).days, buy_date + timedelta(days=1), bought_at,
-                        trade_date + timedelta(days=1), buy_price,
-                        (100 / bought_at) * profit, profit, data[trade_date]['histogram'])
+                        trade_date + timedelta(days=1), current_open,
+                        (100 / bought_at) * profit, profit)
                     break
 
         print "Total buy signals: %s" % total_buy_signals
-        print "Accuracy: PositiveL %s\tNegative: %s\tNeutral: %s" % (n_profit, n_negative, n_neutral)
+        print "Accuracy: Positive: %s\tNegative: %s\tNeutral: %s" % (n_profit, n_negative, n_neutral)
